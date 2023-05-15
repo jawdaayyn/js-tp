2ec0ffa6783cf245c82c772373c180a8a84c331f
Jawdan Akkari <akk.jawdanpro@gmail.com>
2023-05-15 15:32:48 +0200

    -added hints functionnality from aurelien

--- script.js
+++ script.js
@@ -4,48 +4,103 @@ const questions = [
     answers: ["Guy de Maupassant", "Victor Hugo", "Moliere", "Baudelaire"],
     answer: ["Guy de Maupassant"],
     type: "one-choice",
-    hint: "Guy",
+    hint: "C'est un ecrivain francais du 19eme siecle",
   },
- {
+  {
+    question:
+      "Avec quel chanteur Carole Fredericks & Michael Jones ont-il forme un trio ?",
+    answers: [
+      "J.-J. Goldman",
+      "Bernard Lavilliers",
+      "Michel Berger",
+      "Maxime Le Forestie",
+    ],
+    answer: ["J.-J. Goldman"],
+    type: "one-choice",
+    hint: "C'est un chanteur francais",
+  },
+  {
+    question:
+      "Quel conseil regional est preside par Segolene Royal depuis 2004 ?",
+    answers: [
+      "Poitou-Charentes",
+      "Ile de France",
+      " Grand Est",
+      "Pays de la Loire",
+    ],
+    answer: ["Poitou-Charentes"],
+    type: "one-choice",
+    hint: "C'est une region francaise",
+  },
+  {
+    question: "Comment se prenommait la soeur, sculptrice, de Paul Claudel ? ",
+    answers: ["Camille", "Lea", "Marie", "Jeanne"],
+    answer: ["Camille"],
+    type: "one-choice",
+    hint: "C'est un prenom feminin francais",
+  },
+  {
+    question: "A quel pape a succede Jean-Paul II ?",
+    answers: ["Jean-Paul Ier", "Paul VI", "Benoît XVI", "Jean XXIII"],
+    answer: ["Jean-Paul Ier"],
+    type: "one-choice",
+    hint: "C'est un pape",
+  },
+  {
+    question: "En geometrie, combien de cotes possede un losange ?",
+    answers: ["4", "9", "16", "2"],
+    answer: ["4"],
+    type: "one-choice",
+    hint: "C'est un nombre",
+  },
+  {
+    question:
+      "A combien de jeux Olympiques Philippe Candeloro a-t-il gagne des medailles ?",
+    answers: ["2", "5", "1", "3"],
+    answer: ["2"],
+    type: "one-choice",
+    hint: "C'est un nombre",
+  },
+  {
     question: "Avec la Belgique et le Luxembourg, quel pays forme le Benelux ?",
     answers: ["Les Pays-Bas", "Autriche", "Allemagne", "Suede"],
     answer: ["Les Pays-Bas"],
     type: "one-choice",
-    hint: "Guy",
+    hint: "C'est un pays europeen",
   },
   {
     question: "Quelle est la capitale de Bulgarie ?",
     answers: ["Sofia", "Dinant", "Riga", "Lettonie"],
     answer: ["Sofia"],
     type: "one-choice",
-    hint: "Guy",
+    hint: "C'est une ville europeenne",
   },
   {
     question: "De quelle couleur est le cheval blanc d'henri IV ?",
     answer: ["Blanc", "blanc", "BLANC"],
     type: "text",
-    hint: "Guy",
+    hint: "C'est une couleur",
   },
   {
     question: "Quelle est la capitale du Cameroun ? ",
     answers: ["Yaounde", "Dakar", "Kinshasa", "Bamako"],
     answer: ["Yaounde"],
     type: "one-choice",
-    hint: "Guy",
+    hint: "C'est une ville africaine",
   },
   {
     question: "Quel pays est dirige par Kim Jong-il ?",
     answers: ["La Coree du Nord", "La Coree du Sud", "Le Taiwan", "La Chine"],
     answer: ["La Coree du Nord"],
     type: "one-choice",
-    hint: "Guy",
+    hint: "C'est un pays asiatique",
   },
   {
     question: "Cliquez sur les objets qui sont des fruits",
     answers: ["Cerise", "Tomate", "Aubergine", "Banane"],
     answer: ["Cerise", "Tomate", "Banane"],
     type: "multiple-choice",
-    hint: "Guy",
+    hint: "Ce sont des fruits",
   },
 ];
 
@@ -68,19 +123,19 @@ const randomQuestions = (length) => {
   return list;
 };
 
-
-  function showHint(hint) {
+function showHint(hint) {
   const hintElement = document.getElementById("hint");
   hintElement.innerText = "Hint: " + hint;
 }
-  const hintButton = document.createElement("button");
-  hintButton.innerText = "Hint";
-  hintButton.addEventListener("click", () => showHint(currentQuizData.hint));
-  optionsElement.appendChild(hintButton);
-
 
 function loadQuizz(length = 10) {
   currentQuizz = randomQuestions(length);
+  const hintButton = document.createElement("button");
+  hintButton.textContent = "Hint";
+  hintButton.addEventListener("click", () =>
+    showHint(currentQuizz[currentQuestion].hint)
+  );
+  document.getElementById("hint").appendChild(hintButton);
   loadQuestion();
 }
 
